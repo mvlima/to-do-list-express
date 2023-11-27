1436c853bbc742e03e89d1203ffb68671b073204
Marcus Vieira <vieira.mvlima@gmail.com>
2023-11-27 12:02:09 -0300

    included first route

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>My to do List</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
